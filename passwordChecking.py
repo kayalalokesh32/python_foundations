9403e57c2e7e879a9458322fb4ed3fc13093d449
kayalalokesh32 <kayalalokesh32@gmail.com>
2026-01-29 20:23:42 +0530

    Delete passwordChecking.py

--- passwordChecking.py
+++
@@ -1,9 +0,0 @@
-password='9696' #existing password.
-while (True):
-    CheckPassword=input("Enter password :")
-    if (password != CheckPassword):#for checking the password.
-     print("Wrong password, please try again.")
-     continue
-    else :
-     print("Welcome User.")# welcome user.
-     break
